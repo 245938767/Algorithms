aa16d4cbfcec9e3d9d616aa0bf13944309aea04f
LYQ <245938767@qq.com>
2020-01-09 18:38:17 +0800

    shell的错误修复

--- src/cn/xiaodi/ShellSort.java
+++ src/cn/xiaodi/ShellSort.java
@@ -3,14 +3,14 @@ package cn.xiaodi;
 public class ShellSort {
     public ShellSort(int[]a){
         int n=1;
-        while(n<a.length)n=n*3+1;
-        while(n>0) {
-            for (int i = n; i < a.length; i++) {
-                for (int k = n; k > 0; k -= n) {
-                    exch(a, k, k - n);
+        for(int gap=a.length/2;gap>0;gap/=2){
+            for(int k=gap;k<a.length;k++){
+                for(int i=k-gap;i>=0;i=i-gap){
+                    if(a[i]>a[i+gap]){
+                        exch(a,i,i+gap);
+                    }
                 }
             }
-            n=n/3;
         }
     }
     private static void exch(int[]a,int i,int j){
