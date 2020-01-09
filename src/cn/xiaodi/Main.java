91c2fba52e1d1cf66c44901172fda0098dce4a9a
LYQ <245938767@qq.com>
2020-01-09 18:17:33 +0800

    main循环

--- src/cn/xiaodi/Main.java
+++ src/cn/xiaodi/Main.java
@@ -1,4 +1,14 @@
 package cn.xiaodi;
 
 public class Main {
+    public static void main(String[]args){
+        int[]a={5,4,6,9,8,7,3,5,7,5};
+        ShellSort shellSort=new ShellSort(a);
+        SortShow(a);
+    }
+    private static void SortShow(int[]a){
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
 }
