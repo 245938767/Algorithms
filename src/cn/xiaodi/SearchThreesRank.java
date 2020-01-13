204ff273447984a4e586cdc36014672487c4988c
LYQ <245938767@qq.com>
2020-01-13 22:38:39 +0800

    "基于二分查找的map主程序"

--- src/cn/xiaodi/SearchThreesRank.java
+++ src/cn/xiaodi/SearchThreesRank.java
@@ -1,4 +1,11 @@
 package cn.xiaodi;
 
-public class SearchThreesRank {
+public class SearchThreesRank <Key extends Comparable<Key>,Value>{
+    private Key[]keys;
+    private Value[]values;
+    private int N;
+    public SearchThreesRank(int capacity){
+        keys=(Key[])new Comparable[capacity];
+        values=(Value[])new Comparable[capacity];
+    }
 }
